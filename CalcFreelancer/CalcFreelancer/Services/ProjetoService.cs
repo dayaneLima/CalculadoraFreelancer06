103a355e7efef0a62fa17f085f3a068efa25b602
Dayane <dayane.lima.castro@gmail.com>
2018-09-10 19:37:40 -0300

    criação do projeto

---
+++ CalcFreelancer/CalcFreelancer/Services/ProjetoService.cs
@@ -0,0 +1,25 @@
+﻿using CalcFreelancer.Domain.Projetos.Repository;
+using CalcFreelancer.Models;
+using CalcFreelancer.Repository;
+using CalcFreelancer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcFreelancer.Services
+{
+    public class ProjetoService: IProjetoService
+    {
+        private readonly IProjetoRepository ProjetoRepository;
+
+        public ProjetoService(IProjetoRepository projetoRepository)
+        {
+            ProjetoRepository = projetoRepository;
+        }
+
+        public void Inserir(Projeto projeto)
+        {
+            ProjetoRepository.Insert(projeto);
+        }
+    }
+}
